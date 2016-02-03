ae24b51ce07fbbcc0b40f588ee3e51b534cfbbdb
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-02-03 17:54:55 +0100

    Add simple baseline param generator for events.

---
+++ code/baselines/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='nnp_baselines',
+    version='0.1',
+    py_modules=['event_baseline'],
+    install_requires=[
+        'Click', 'numpy'
+    ],
+    entry_points='''
+        [console_scripts]
+        event_baseline=event_baseline:run
+    '''
+)
+
