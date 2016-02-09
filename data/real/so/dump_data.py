ee381611dace0d812de94c43e6baa15bf5273bf9
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-02-09 21:53:54 +0100

    Update SO data.

--- data/real/so/dump_data.py
+++ data/real/so/dump_data.py
@@ -1,17 +1,78 @@
+from __future__ import print_function
 import psycopg2 as pg
 import getpass as G
+from collections import namedtuple
 
 output_events = 'events.txt'
 output_time = 'time.txt'
+output_userids = 'userids.txt'
+output_badge_labels = 'badges.csv'
 
-with pg.connect(database='stackexchange', user='utkarsh', password=G.getpass('DB password: '), host='psql-science') as conn:
-    cur = conn.cursor()
-    # Ordering is important for mapping results back to the data, if needed.
-    cur.execute('''SELECT EventIds, Timestamp FROM so_data ORDER BY userid''')
+SO_events = namedtuple('SO_events', ['times', 'events', 'badge_map', 'userids'])
 
-    with open(output_events, 'w') as f_events, open(output_time, 'w') as f_time:
-        for row in cur:
-            events, times = row[0], row[1]
-            f_events.write(','.join(str(x) for x in events) + '\n')
-            # Can change times to something more granular than seconds.
-            f_time.write(','.join(str(x) for x in times) + '\n')
+def write():
+    try:
+        with pg.connect(database='stackexchange', user='utkarsh', password=G.getpass('DB password: '), host='psql-science') as conn:
+            cur = conn.cursor()
+            # Ordering is important for mapping results back to the data, if needed.
+            cur.execute('''SELECT userid, BadgeNames, Timestamp FROM so_data ORDER BY userid''')
+
+            badge_map = {}
+            badge_count = 1
+            userids = []
+
+            with open(output_events, 'w') as f_events, open(output_time, 'w') as f_time:
+                for row in cur:
+                    userid, events, times = row[0], row[1], row[2]
+                    if len(set(times)) != len(times):
+                        # If there are any repeated events, just skip the user.
+                        continue
+
+                    userids.append(userid)
+                    min_t = min(times)
+                    event_ids = []
+                    for badge in events:
+                        if badge not in badge_map:
+                            badge_map[badge] = badge_count
+                            badge_count += 1
+                        event_ids.append(badge_map[badge])
+
+                    f_events.write(' '.join(str(x) for x in event_ids) + '\n')
+                    # Can change times to something more granular than seconds.
+                    f_time.write(' '.join(str(x - min_t) for x in times) + '\n')
+
+            with open(output_userids, 'w') as f_userids:
+                f_userids.write('userid\n')
+                f_userids.writelines([str(x) + '\n' for x in userids])
+
+            with open(output_badge_labels, 'w') as f_badges:
+                f_badges.write('id, badge\n')
+                for badge in badge_map:
+                    f_badges.write('{}, {}\n'.format(badge_map[badge], badge))
+    except pg.OperationalError:
+        print('Not running on DB.')
+
+def read_events():
+    with open(output_events) as f_events:
+        events = [[int(y) for y in x.split()] for x in f_events]
+
+    with open(output_time) as f_times:
+        times = [[float(y) for y in x.split()] for x in f_times]
+
+    with open(output_userids) as f_userids:
+        next(f_userids)
+        userids = [int(x) for x in f_userids]
+
+    badge_map = {}
+    with open(output_badge_labels) as f_badge_labels:
+        next(f_badge_labels)
+        for row in f_badge_labels:
+            id, name = row.split(',')
+            badge_map[int(id)] = name.strip()
+
+    return SO_events(events=events, times=times, badge_map=badge_map, userids=userids)
+
+if __name__ == '__main__':
+    write()
+
+
--- data/real/so/query.sql
+++ data/real/so/query.sql
@@ -30,11 +30,12 @@ CREATE INDEX badge_type_id_idx ON badgeTypes(Id);
 CREATE INDEX badge_type_badge_idx ON badgeTypes(badge);
 
 
-DROP VIEW IF EXISTS relevant_users;
+DROP VIEW IF EXISTS relevant_users CASCADE;
 CREATE VIEW relevant_users AS
     SELECT userid
     FROM badges_count BC1
     WHERE date BETWEEN '2012-01-01' AND '2014-01-01'
+          AND name IN ('Favourite Question','Stellar Question','Nice Question','Good Question','Great Question','Popular Question','Notable Question','Famous Question','Enlightened','Guru','Nice Answer','Good Answer','Great Answer','Populist','Reversal','Revival','Necromancer','Activist','Campaigner','Founder','Good Question','Grassroots','Great Question','Movement','Nice Question','Promoter','Revolution','Caucus','Constituent','Yearling','Announcer','Booster','Publicist')
     GROUP BY userid
     HAVING count(*) > 40;
 
@@ -45,27 +46,29 @@ CREATE VIEW relevant_badges AS
     FROM badges_count BC2
     WHERE EXISTS (SELECT * FROM relevant_users RU1 WHERE RU1.userid = BC2.userid)
           AND date BETWEEN '2012-01-01' AND '2014-01-01'
+          -- These are the badges which can be awareded multiple times
+          AND name IN ('Favourite Question','Stellar Question','Nice Question','Good Question','Great Question','Popular Question','Notable Question','Famous Question','Enlightened','Guru','Nice Answer','Good Answer','Great Answer','Populist','Reversal','Revival','Necromancer','Activist','Campaigner','Founder','Good Question','Grassroots','Great Question','Movement','Nice Question','Promoter','Revolution','Caucus','Constituent','Yearling','Announcer','Booster','Publicist')
     GROUP BY name
     HAVING count(*) > 100
     ORDER BY name;
 
-
-EXPLAIN
-SELECT count(*)               AS entries,
-       count(distinct userid) AS num_users,
-       count(distinct name)   AS num_badges
-FROM badges_count BC3
-WHERE EXISTS (SELECT * FROM relevant_badges RB1 WHERE RB1.name = BC3.name)
-    AND EXISTS (SELECT * FROM relevant_users RU2 WHERE RU2.userid = BC3.userid)
-    AND BC3.date BETWEEN '2012-01-01' AND '2014-01-01';
+-- EXPLAIN
+-- SELECT count(*)               AS entries,
+--        count(distinct userid) AS num_users,
+--        count(distinct name)   AS num_badges
+-- FROM badges_count BC3
+-- WHERE EXISTS (SELECT * FROM relevant_badges RB1 WHERE RB1.name = BC3.name)
+--     AND EXISTS (SELECT * FROM relevant_users RU2 WHERE RU2.userid = BC3.userid)
+--     AND BC3.date BETWEEN '2012-01-01' AND '2014-01-01';
 
 
 DROP TABLE IF EXISTS so_data;
 CREATE TABLE so_data AS (
     SELECT userid,
-           array_agg(BC3.name)                     AS BadgeNames,
-           array_agg(BT.id)                        AS EventIds,
-           array_agg(EXTRACT(EPOCH FROM BC3.date)) AS TimeStamp
+           array_agg(BC3.name ORDER BY BC3.date)                     AS BadgeNames,
+           array_agg(BT.id ORDER BY BC3.date)                        AS EventIds,
+           array_agg(EXTRACT(EPOCH FROM BC3.date) ORDER BY BC3.date) AS TimeStamp,
+           count(*)                                                  AS NumEvents
     FROM badges_count BC3
     JOIN badgeTypes BT ON (BT.badge = BC3.name)
     WHERE EXISTS (SELECT * FROM relevant_badges RB1 WHERE RB1.name = BC3.name)
@@ -73,3 +76,5 @@ CREATE TABLE so_data AS (
         AND BC3.date BETWEEN '2012-01-01' AND '2014-01-01'
     GROUP BY BC3.userid
 );
+
+
